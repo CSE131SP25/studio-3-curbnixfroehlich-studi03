e2412c4d0ff71308818b85e3b6437939fba5eb65
susannanix <susannanix@localhost>
2025-02-05 15:48:53 -0600

    Finished :)

---
+++ src/studio3/SieveofEratosthenes.java
@@ -0,0 +1,41 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class SieveofEratosthenes {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n;	
+		System.out.println("What is the value n?");
+		n = scan.nextInt();
+		
+		boolean [] seive;
+		seive = new boolean [n];
+		
+		for ()
+		
+		for ( int i = 0; i < (n - 1); i++) {
+			
+			seive[i] = i + 2;
+			System.out.println(seive[i]);
+			
+		}
+		
+		for ( int j = 0; j < ( n - 1 ); j++) {
+			
+			if 
+			
+			for ( int k = 2; k = 2j); k++) {
+				
+			}
+			
+		}
+		
+		
+
+	}
+
+}
